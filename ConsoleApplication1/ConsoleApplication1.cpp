635b44c052adb5f848a28a85d34b91eae5a3b895
Andee13 <39676479+Andee13@users.noreply.github.com>
2018-12-19 00:31:05 +0200

    4554

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -23,26 +23,41 @@ public:
 	void inputDataFromFile(ifstream &in) {
 		in >> p;
 		in >> V;
-		if (V < 8 && V>20) {
+		if (V < 8 || V > 20) {
+			cout << "d is not in interval, correct it and start program" << endl;
+			exit(0);
+		}
+		in >> d;
+		if (d < 0.001 || d > 0.003) {
 			cout << "d is not in interval, correct it and start program" << endl;
 			exit(0);
 		}
 		in >> S;
-		in >> d;
-		if (d<0.001 && d > 0.003) {
-			cout << "d is not in interval, correct it and start program" << endl;
-			exit(0);
-		}
 		in >> u;
 	}
 	void outputDataToFile(ofstream &out) {
-		out <<"V = "<< V <<" S = " << S << " u = "<< u <<" Re = " << Re <<" p = "<< p << " d = "<< d << " fi = "<< fi << " calculatedobj = "<< calculatedParametr<< endl;
+		if (fi != NULL) {
+			out << "V = " << V << " S = " << S << " u = " << u << " Re = " << Re << " p = " << p << " d = " << d << " fi = " << fi << " calculatedobj = " << calculatedParametr << endl;
+		}
+		else{
+			out << "object can't be calculated" << endl;
+		}
 	}
 	void print() {
-		cout << "Data from file:\np = " << p << " V = " << V << " S = " << S << " d = " << d << " u = " << u << endl;
-		cout << "Data after calculation:\n Re = " << Re << " fi = " << fi << " calculatedParametr = " << calculatedParametr << endl;
+		if (fi != NULL) {
+			cout << "Data Input:\np = " << p << " V = " << V << " S = " << S << " d = " << d << " u = " << u << endl;
+			cout << "Data after calculation:\n Re = " << Re << " fi = " << fi << " calculatedParametr = " << calculatedParametr << endl;	
+		}
+		if (fi == NULL) {
+			cout << "Object can't be calculated as fi = NULL" << endl;
+		}
 	}
 	void calculate() {
+		if (V < 8 || V > 20) {
+			fi = NULL;
+			cout << "V isn't in interval" << endl;
+			return;
+		}
 		if (u != 0) {
 			Re = (V * d * p) / u;
 			if (Re <= 2) {
@@ -66,6 +81,9 @@ public:
 		else {
 			cout << "Function with this data does not calculated fi = NULL" << endl;
 		}
+	}
+	void operator +=(int a) {
+		V += a;
 	}
 	void operator = (ResistanceForce &v) {
 		V = v.V;
@@ -110,6 +128,7 @@ public:
 		}
 		return false;
 	}
+
 	ResistanceForce() {}
 	ResistanceForce(double square, double volume, double p, double r, double u) :S(square),V(volume),p(p), d(r), u(u){}
 	ResistanceForce(const ResistanceForce &obj) {
@@ -137,15 +156,18 @@ int main()
 	//constructors
 	ResistanceForce obj1(), obj2(2,4,6,3,8);
 	//Calculation of function
-	ResistanceForce ar[13];
+	ResistanceForce ar[10];
 	ar[0].inputDataFromFile(in);
 	ar[0].calculate();
 	ar[0].print();
 	ar[0].outputDataToFile(out);
-	for (int i = 1; i < 13; i++) {
+	double v;
+	cout << "Enter increment for V " << endl;
+	cin >> v;
+	for (int i = 1; i < 10; i++) {
 		cout << "object "<< i << endl;
 		ar[i] = ar[i-1];
-		ar[i].V += 1;
+		ar[i].V += v;
 		ar[i].calculate();
 		ar[i].print();
 		ar[i].outputDataToFile(out);
